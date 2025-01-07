ded300376f0a8237627eb58f21f2f06f25e0b09e
657 <kuyermqi@outlook.com>
2025-01-08 00:27:12 +0800

    build: update build config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'react-color4bg',
+      formats: ['es'],
       fileName: 'react-color4bg',
     },
     rollupOptions: {
